d232000f59831c4d9307ec307cc24da8f50c0074
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-03 21:58:06 -0300

    typing-files

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,20 +1,18 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "../Pages/HomePage"
+import { HomePage } from "../Pages/HomePage";
 import { PokedexPage } from "../Pages/PokedexPage";
 import { PokemonDetailsPage } from "../Pages/PokemonDetailsPage";
 import { ErrorPage } from "../Pages/ErrorPage";
 
-export const Router = () => {
+export const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="page/:pageNumber" element={<HomePage />} />
         <Route path="pokedex" element={<PokedexPage />} />
-        <Route
-          path="pokemon/:pokemon"
-          element={<PokemonDetailsPage></PokemonDetailsPage>}
-        />
+        <Route path="pokemon/:pokemon" element={<PokemonDetailsPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
