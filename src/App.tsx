57411a5c70dcd29a5eb93d9bbe0a3ce0d166d687
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-04 06:55:52 -0300

    using-chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { Router } from './Routes/Router';
+import theme from './theme';
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
 
 function App() {
   return (
     <div className="App">
+        <ChakraProvider resetCSS theme={theme}>
       <h1> Hello, World!</h1>
       <Router />
-
+      </ChakraProvider>
     </div>
   );
 }
